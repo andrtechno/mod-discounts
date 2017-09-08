7d31e9341f0da83526cd466f79a0a63674595f74
andrtechno <andrew.panix@gmail.com>
2017-09-08 16:35:27 +0300

    add migrate

--- migrations/m170908_104527_init.php
+++ migrations/m170908_104527_init.php
@@ -20,12 +20,12 @@ class m170908_104527_init extends Migration {
             'switch' => $this->boolean()->defaultValue(1),
                 ], $tableOptions);
 
-        
+
         $this->createTable('{{%shop_discount_category}}', [
             'id' => $this->primaryKey(),
             'discount_id' => $this->integer(),
             'category_id' => $this->integer(),
-        ]);
+        ], $tableOptions);
 
 
 
@@ -33,27 +33,24 @@ class m170908_104527_init extends Migration {
             'id' => $this->primaryKey(),
             'discount_id' => $this->integer(),
             'manufacturer_id' => $this->integer(),
-        ]);
-        
-        
+        ], $tableOptions);
+
+
         $this->createIndex('switch', '{{%shop_discount}}', 'switch', 0);
         $this->createIndex('start_date', '{{%shop_discount}}', 'start_date', 0);
         $this->createIndex('end_date', '{{%shop_discount}}', 'end_date', 0);
-        
+
         $this->createIndex('discount_id', '{{%shop_discount_category}}', 'discount_id', 0);
         $this->createIndex('category_id', '{{%shop_discount_category}}', 'category_id', 0);
-        
+
         $this->createIndex('discount_id', '{{%shop_discount_manufacturer}}', 'discount_id', 0);
         $this->createIndex('manufacturer_id', '{{%shop_discount_manufacturer}}', 'manufacturer_id', 0);
-        
-        
     }
 
     public function down() {
         $this->dropTable('{{%shop_discount}}');
         $this->dropTable('{{%shop_discount_category}}');
         $this->dropTable('{{%shop_discount_manufacturer}}');
-
     }
 
 }
