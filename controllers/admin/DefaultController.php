08b9b41b91c7c753c001bd40d0a228c9f506959e
Andrew <andrew.panix@gmail.com>
2019-09-14 13:31:47 +0300

    init

--- controllers/admin/DefaultController.php
+++ controllers/admin/DefaultController.php
@@ -91,10 +91,7 @@ class DefaultController extends AdminController
 
             if ($model->validate()) {
                 $model->save();
-                Yii::$app->session->setFlash('success', Yii::t('app', ($isNew) ? 'SUCCESS_CREATE' : 'SUCCESS_UPDATE'));
-                $redirect = (isset($post['redirect'])) ? $post['redirect'] : Yii::$app->request->url;
-                if (!Yii::$app->request->isAjax)
-                    return $this->redirect($redirect);
+                $this->redirectPage($isNew, $post);
             }
         }
 
