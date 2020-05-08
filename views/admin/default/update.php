2144a703a8a4a376a0bdce5e8ea6ab964843c88e
Andrew <andrew.panix@gmail.com>
2020-05-08 23:54:16 +0300

    init

--- views/admin/default/update.php
+++ views/admin/default/update.php
@@ -6,11 +6,12 @@ use yii\helpers\ArrayHelper;
 use panix\mod\shop\models\Manufacturer;
 use panix\engine\jui\DatetimePicker;
 use panix\mod\shop\models\Category;
+
 /**
  * @var \panix\mod\discounts\models\Discount $model
  * @var \panix\engine\bootstrap\ActiveForm $form
  */
-$form = ActiveForm::begin(['id'=>'discount-form']);
+$form = ActiveForm::begin(['id' => 'discount-form']);
 
 ?>
 <div class="card">
@@ -20,8 +21,8 @@ $form = ActiveForm::begin(['id'=>'discount-form']);
     <div class="card-body">
         <?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
         <?= $form->field($model, 'sum')->textInput(['maxlength' => 10]) ?>
-        <?= $form->field($model, 'start_date')->widget(DatetimePicker::class, [])->textInput(['maxlength' => 19,'autocomplete'=>'off']) ?>
-        <?= $form->field($model, 'end_date')->widget(DatetimePicker::class, [])->textInput(['maxlength' => 19,'autocomplete'=>'off']) ?>
+        <?= $form->field($model, 'start_date')->widget(DatetimePicker::class, [])->textInput(['maxlength' => 19, 'autocomplete' => 'off']) ?>
+        <?= $form->field($model, 'end_date')->widget(DatetimePicker::class, [])->textInput(['maxlength' => 19, 'autocomplete' => 'off']) ?>
 
         <?= $form->field($model, 'manufacturers')
             ->dropDownList(ArrayHelper::map(Manufacturer::find()->all(), 'id', 'name'), [
@@ -29,54 +30,53 @@ $form = ActiveForm::begin(['id'=>'discount-form']);
                 'multiple' => 'multiple'
             ])->hint('Чтобы скидка заработала, необходимо указать категорию');
         ?>
-        <div class="form-group row2">
-            <div class="alert alert-info"><?= Yii::t('discounts/default', "CATEGORY_INFO"); ?></div>
-        </div>
-
         <div class="form-group row">
-            <div class="col-sm-4">
-                <?= Html::label(Yii::t('app/default', 'Поиск:'), 'search-discount-category', ['class' => 'control-label']); ?>
+            <div class="col-sm-4 col-lg-2">
+                <?= Html::label(Yii::t('discounts/Discount','CATEGORIES')); ?>
+
             </div>
-            <div class="col-sm-8">
+            <div class="col-sm-8 col-lg-10">
+                <?= Html::label(Yii::t('app/default', 'Поиск:'), 'search-discount-category', ['class' => 'control-label']); ?>
                 <?= Html::textInput('search', null, [
                     'id' => 'search-discount-category',
                     'class' => 'form-control',
                     'onChange' => '$("#CategoryTree").jstree("search", $(this).val());'
                 ]); ?>
+                <br/>
+                <?php
+                echo \panix\ext\jstree\JsTree::widget([
+                    'id' => 'CategoryTree',
+                    'allOpen' => true,
+                    'data' => Category::find()->dataTree(1, null, ['switch' => 1]),
+                    'core' => [
+                        'strings' => [
+                            'Loading ...' => Yii::t('app/default', 'LOADING')
+                        ],
+                        'check_callback' => true,
+                        "themes" => [
+                            "stripes" => true,
+                            'responsive' => true,
+                            "variant" => "large",
+                            // 'name' => 'default-dark',
+                            // "dots" => true,
+                            // "icons" => true
+                        ],
+                    ],
+                    'plugins' => ['search', 'checkbox'],
+                    'checkbox' => [
+                        'three_state' => false,
+                        "keep_selected_style" => false,
+                        'tie_selection' => false,
+                    ],
+                ]);
+
+                foreach ($model->getCategories() as $id) {
+
+                    $this->registerJs("$('#CategoryTree').checkNode({$id});", yii\web\View::POS_END, "checkNode{$id}");
+                }
+
+                ?>
             </div>
-            <?php
-            echo \panix\ext\jstree\JsTree::widget([
-                'id' => 'CategoryTree',
-                'allOpen' => true,
-                'data' => Category::find()->dataTree(1, null, ['switch' => 1]),
-                'core' => [
-                    'strings' => [
-                        'Loading ...' => Yii::t('app/default', 'LOADING')
-                    ],
-                    'check_callback' => true,
-                    "themes" => [
-                        "stripes" => true,
-                        'responsive' => true,
-                        "variant" => "large",
-                        // 'name' => 'default-dark',
-                        // "dots" => true,
-                        // "icons" => true
-                    ],
-                ],
-                'plugins' => ['search', 'checkbox'],
-                'checkbox' => [
-                    'three_state' => false,
-                    "keep_selected_style" => false,
-                    'tie_selection' => false,
-                ],
-            ]);
-
-            foreach ($model->getCategories() as $id) {
-
-                $this->registerJs("$('#CategoryTree').checkNode({$id});", yii\web\View::POS_END, "checkNode{$id}");
-            }
-
-            ?>
         </div>
 
     </div>
